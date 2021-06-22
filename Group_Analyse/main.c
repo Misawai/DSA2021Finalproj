694884ce9d516f6bfb7cd1cae2ee67c8ea88d528
Misawai <69676474+Misawai@users.noreply.github.com>
2021-06-22 18:36:43 +0800

    if multiple queries occurrs, weighted union is needed.

--- Group_Analyse/main.c
+++ Group_Analyse/main.c
@@ -5,9 +5,16 @@ int main()
 	int n_mails, n_queries;
 	mail *mails;
 	query queries;
-	void api.init(int *n_mails, int *n_queries, mail **mails, query **queries);//init
+	api.init(&n_mails, &n_queries, &mails, &queries);//init
 	//using disjoinnt set to implememt
 	//For two different people, if A sends a mail to B in the mail set, then A is related to B and B is related to A.
 	//struct int len(size of the set) int mids[512](id's)
 	
+}
+void Group_Analysis()
+{
+	//disjoint set noneed to compressed since only query once or....
+	//if many queries impelemented, path compression is needed.
+	//
+	struct 
 }
