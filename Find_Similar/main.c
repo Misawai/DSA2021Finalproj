59b500353a2c387d8b1f49fa75a3ac56c5126cee
蔡孟辰 <b06502020@ntu.edu.tw>
2021-06-27 23:31:26 +0800

    able to run but WA

--- Find_Similar/main.c
+++ Find_Similar/main.c
@@ -3,13 +3,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <math.h>
+#include <time.h>
 
 #define TOKEN_MAX_LEN 80
+#define HASH_SIZE 80000
 
 /* Definition of 'drawer' type for hashing purpose */
 typedef struct Drawer{
     bool yes;
     char* token_address;
+    int token_len;
     struct Drawer* next;
 }drawer;
 
@@ -18,6 +22,7 @@ drawer* createDrawer(){
     drawer* new_d = malloc(sizeof(drawer));
     new_d->yes = false;
     new_d->token_address = NULL;
+    new_d->token_len = 0;
     new_d->next = NULL;
 
     return new_d;
@@ -78,16 +83,18 @@ int hash_value(char c){
     else                  return ascii - 87;
 }
 
-bool is_valid_match(drawer* drawr_head, char* buff, int str_len){
+bool is_valid_match(drawer* drawer_head, char* buff, int str_len){
     // Check string match or not in entire drawer linked-list
-    drawer_curr = drawer_head;
+    drawer* drawer_curr = drawer_head;
+    bool check;
     while (drawer_curr != NULL){
         char* check_str = drawer_curr->token_address;
-        bool check = false;
-        for (int k = 0; k < buff_i; k++){
+        int check_str_len = drawer_curr->token_len;
+        for (int k = 0; k < str_len; k++){
             char c = check_str[k];
+            check = false;
             // Size are different
-            if (!isdigit(c) && !isdigit(c)){
+            if (check_str_len != str_len){
                 break; 
             }
             // char are different
@@ -105,11 +112,14 @@ bool is_valid_match(drawer* drawr_head, char* buff, int str_len){
 
 void construct_hash_table(drawer** hash_table, tokenList* tl, 
                           mail target_mail, int* A){
-    // TODO:  
+    // Initialize all the drawers in the table
+    for (int i = 0; i < HASH_SIZE; i ++){
+        hash_table[i] = createDrawer();
+    }
     // Get token head
-    token* cur_token = tl->head;
+    token* curr_token = tl->head;
     // Subject and Content
-    char** source_list[2];
+    char* source_list[2];
     source_list[0] = target_mail.subject;
     source_list[1] = target_mail.content;
     int len_list[] = {256, 100000};
@@ -128,48 +138,52 @@ void construct_hash_table(drawer** hash_table, tokenList* tl,
                 // put in buffer
                 buff[buff_i++] = c;
                 // compute hash
-                c_hash += hash_value(c); 
+                c_hash += (int)pow(hash_value(c), 2); 
             } 
             // Check hash value and store in hash table & token list
             else{
-                // Put in token list
-                token* new_t = createToken(buffer, buff_i);
-                curr_token->next = new_t;
-                curr_token = new_t; 
-                // Get corresponding drawer
-                drawer* corres_drawer = hash_table[c_hash];
-                // No collision
-                if (corres_drawer->yes == false){
-                    corres_drawer->yes = true;
-                    corres_drawer->token_address = curr_token->str;  
-                    // update token set A size
-                    *A = *A + 1;
-                }
-                // Collision occur, check collision type
-                else{
-                    // Token has occured before
-                    if (is_valid_match(corres_drawer, buff, buff_i)){
-                        // do nothing
-                    } 
-                    // New token (with same hash value)
-                    else{
-                        // open a new drawer at the end of list
-                        drawer* temp = corres_drawer;
-                        while(temp->next != NULL) temp = temp->next;
-                        temp->next = createDrawer();
-                        temp->next->yes = true;
-                        temp->next->token_address = curr_token->str;
+                if (buff_i > 0){
+                    // Put in token list
+                    token* new_t = createToken(buff, buff_i);
+                    curr_token->next = new_t;
+                    curr_token = new_t; 
+                    // Get corresponding drawer
+                    drawer* corres_drawer = hash_table[c_hash];
+                    // No collision
+                    if (corres_drawer->yes == false){
+                        corres_drawer->yes = true;
+                        corres_drawer->token_address = curr_token->str;  
+                        corres_drawer->token_len = buff_i;  
                         // update token set A size
                         *A = *A + 1;
                     }
+                    // Collision occur, check collision type
+                    else{
+                        // Token has occured before
+                        if (is_valid_match(corres_drawer, buff, buff_i)){
+                            // do nothing
+                        } 
+                        // New token (with same hash value)
+                        else{
+                            // open a new drawer at the end of list
+                            drawer* temp = corres_drawer;
+                            while(temp->next != NULL) temp = temp->next;
+                            temp->next = createDrawer();
+                            temp->next->yes = true;
+                            temp->next->token_address = curr_token->str;
+                            temp->next->token_len = buff_i;
+                            // update token set A size
+                            *A = *A + 1;
+                        }
+                    }
+                    // Clear the buffer
+                    for (int k = 0; k < buff_i; k++){
+                        buff[k] = '\0';
+                    } 
+                    buff_i = 0;
+                    // Clear hash
+                    c_hash = 0;
                 }
-                // Clear the buffer
-                for (int k = 0; k < buff_i; k++){
-                    buff[k] = '\0';
-                } 
-                buff_i = 0;
-                // Clear hash
-                c_hash = 0;
             }
         }
     }
@@ -186,13 +200,14 @@ void check_intersect(drawer** hash_table, mail current_mail, char flag,
 
     // Subject or Content
     int max_size;
+    char* source;
     if (flag == 's') {
         max_size = 256;
-        char* source = current_mail.subject;
+        source = current_mail.subject;
     }
     if (flag == 'c') {
         max_size = 100000;
-        char* source = current_mail.content;
+        source = current_mail.content;
     }
     for (int i = 0; i < max_size; i++){
         // Initialization
@@ -202,20 +217,20 @@ void check_intersect(drawer** hash_table, mail current_mail, char flag,
             // put in buffer
             buff[buff_i++] = c;
             // compute hash
-            c_hash += hash_value(c); 
+            c_hash += (int) pow(hash_value(c), 2); 
         } 
         else{
             // Form a token only when there's item in buffer
             if (buff_i > 0){
                 // Update set B size
-                B++;
+                *B += 1;
                 // check hash table
                 if (hash_table[c_hash]->yes){
                     // check strings
                     bool check = is_valid_match(hash_table[c_hash], 
                                                 buff, buff_i);
                     // if match, update size of intersection
-                    if (check) inter_size++;
+                    if (check) *inter_size += 1;
                 }
                 // Clear the buffer
                 for (int k = 0; k < buff_i; k++){
@@ -230,20 +245,23 @@ void check_intersect(drawer** hash_table, mail current_mail, char flag,
 }
     
 void _find_similar(mail* mails, int n_mails, int mid, 
-                  int threshold, int* ans_array){
+                  double threshold, int* ans_array, int* ans_i){
     /* This function will output the mail IDs which exceed the 
     * threshold value in the ans_array in increasing order*/
 
     // Construct the hash tablle for mail[mid]
     // and record size of the token set
-    int h_size = 80000;
     int token_size_A;
-    drawer* hash_table[h_size];
+    drawer* hash_table[HASH_SIZE];
     tokenList* tl = createTokenList();
+    clock_t cons_start = clock();
     construct_hash_table(hash_table, tl, mails[mid], &token_size_A);
+    clock_t cons_end = clock();
+    printf("construct hash table cost %f s\n", 
+            (double) (cons_end - cons_start) / CLOCKS_PER_SEC); 
 
+    clock_t check_start = clock();
     // Check similarity from 0 to n_mails
-    int ans_i = 0;
     for (int i = 0; i < n_mails; i++){
         // Skip mid
         if (i == mid) continue;
@@ -259,12 +277,43 @@ void _find_similar(mail* mails, int n_mails, int mid,
 
         // Determine whether to put in answer array
         int union_size = token_size_A + token_size_B - inter_size;
-        if ( (float) union_size/inter_size > threshold){
-            ans_array[ans_i++] = mails[i].id;
+        double score = (double) inter_size / union_size; 
+        //printf("score: %f, threshold: %f\n", score, threshold);
+        if (score > threshold){
+            ans_array[*ans_i] = mails[i].id;
+            *ans_i += 1;
         }
     }
+
+    clock_t check_end = clock();
+    printf("check intersection cost %f s\n", 
+            (double) (check_end - check_start) / CLOCKS_PER_SEC); 
 }
 
+int n_mails, n_queries;
+mail *mails;
+query *queries;
+
 int main(){
+	api.init(&n_mails, &n_queries, &mails, &queries);
+    int i = 0, j = 0;
+    for (int i = 0; i < n_queries; i++){
+        if(queries[i].type == find_similar){
+            int ans_array[10000];
+            int ans_i = 0;
+            int mid = queries[i].data.find_similar_data.mid;
+            double thresh = queries[i].data.find_similar_data.threshold;
+            printf("mid: %d, thresh: %f\n", mid, thresh);
+            _find_similar(mails, n_mails, mid, 
+                          thresh, ans_array, &ans_i);
+            //printf("finish a query\n");
+            //api.answer(queries[i].id, ans_array, ans_i);
+            printf("4: ");
+            for (int j = 0; j < ans_i; j++){
+                printf("%d ", ans_array[j]);
+            }
+            return 0;
+        }
+    }
     return 0;
 }
